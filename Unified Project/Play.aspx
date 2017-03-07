b88462efe5029a6fdca9c64f3c6b53756f19f313
Caleb Hillhouse <calebinski94@gmail.com>
2017-03-07 14:19:05 -0600

    First upload.

---
+++ Unified Project/Play.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Play.aspx.cs" Inherits="Play" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Commerce Budgeting Tool</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/styles.css" rel="stylesheet"/>
+	<link href="css/fonts.css" rel="stylesheet"/>
+</head>
+<body>
+     <div class="container">
+        <!-- Navigation -->
+			<div class="col-sm-2">
+				<div class="row">
+                    <a href="Play.aspx">
+					    <div class="navButton" style="background-color: #6ab244;">
+						    <p>Play</p>
+					    </div>
+                    </a>
+				</div>
+				<div class="row">
+					<a href="Wallet.aspx">
+					    <div class="navButton">
+						<p>Wallet</p>
+					    </div>
+                    </a>
+				</div>
+				<div class="row">
+					<a href="Goals.aspx">
+					    <div class="navButton">
+						<p>Goals</p>
+					    </div>
+                    </a>
+				</div>
+				<div class="row">
+					<a href="Budget.aspx">
+					    <div class="navButton">
+						<p>Budget</p>
+					    </div>
+                    </a>
+				</div>
+				<div class="row">
+					<a href="Help.aspx">
+					    <div class="navButton">
+						<p>Help</p>
+					    </div>
+                    </a>
+				</div>
+			</div>
+    <!-- Content -->
+        <div class="col-sm-10">
+            <div class="content">
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+                </div>
+            </div>
+         </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+	<script src="bootstrap/js/jquery-3.1.1.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ Unified Project/js/populateGoals.js
@@ -0,0 +1,93 @@
+﻿var json = [{
+    "goalID": "Retirement",
+    "CurrentAmount": "59999",
+    "TotalAmount": "60000"
+},
+{
+    "goalID": "New Car",
+    "CurrentAmount": "0",
+    "TotalAmount": "5000"
+},
+{
+    "goalID": "Vacation",
+    "CurrentAmount": "1200",
+    "TotalAmount": "3000"
+}];
+
+
+for (var i = 0; i < json.length; i++) {
+    
+    //create outer div
+    var divSectionTitle = document.createElement('div');
+    divSectionTitle.className = 'sectionTitle';
+    document.getElementById('content').appendChild(divSectionTitle);
+
+    //bootstrap grid div for goal id
+    var divColGoalID = document.createElement('div');
+    divColGoalID.className = 'col-sm-4';
+    divSectionTitle.appendChild(divColGoalID);
+    
+    //display goal id
+    var divGoalID = document.createElement('div');
+    divGoalID.className = 'textLeft';
+    divColGoalID.appendChild(divGoalID);
+    divGoalID.innerHTML = "<h3>" + json[i].goalID + "</h3>";
+
+    //bootstrap grid div for motivational message
+    var divColMotMsg = document.createElement('div');
+    divColMotMsg.className = 'col-sm-4';
+    divSectionTitle.appendChild(divColMotMsg);
+
+    //display motivational message
+    var divMotMsg = document.createElement('div');
+    divMotMsg.className = 'textCenter';
+    divColMotMsg.appendChild(divMotMsg);
+    divMotMsg.setAttribute('style', 'padding-top: 1em;')
+
+    var progress = json[i].CurrentAmount / json[i].TotalAmount;
+
+    if (progress < .25) {
+        divMotMsg.innerHTML = "<h4>You've added a new goal.</h4>";
+    } else if (progress < .5 && progress > .25) {
+        divMotMsg.innerHTML = "<h4>Making Progress!</h4>";
+    } else if (progress < .75 && progress > .5) {
+        divMotMsg.innerHTML = "<h4>You're doing great!</h4>";
+    } else if (progress < 1 && progress > .75) {
+        divMotMsg.innerHTML = "<h4>Almost there!</h4>";
+    } else if (progress == 1) {
+        divMotMsg.innerHTML = "<h4>You've reached your goal!</h4>";
+    }
+
+    //bootstrap grid div for total amount
+    var divColTotalAmt = document.createElement('div');
+    divColTotalAmt.className = 'col-sm-4';
+    divSectionTitle.appendChild(divColTotalAmt);
+
+    //display total amount
+    var divTotalAmt = document.createElement('div');
+    divTotalAmt.className = 'textRight';
+    divColTotalAmt.appendChild(divTotalAmt);
+    divTotalAmt.innerHTML = "<h3>" + json[i].TotalAmount + "</h3>";
+
+    //progress bar
+    var divProgressWrapper = document.createElement('div');
+    divProgressWrapper.className = 'progressBarWrapper';
+    document.getElementById('content').appendChild(divProgressWrapper);
+
+    var divProgressBar = document.createElement('div');
+    divProgressBar.className = 'progressBar';
+    divProgressWrapper.appendChild(divProgressBar);
+
+    var divProgressFiller = document.createElement('div');
+    divProgressFiller.className = 'progressBarFiller';
+    divProgressBar.appendChild(divProgressFiller);
+    divProgressFiller.setAttribute('style', 'width: ' + (progress * 100) + '%');
+    if (progress == 1) {
+        divProgressFiller.setAttribute('style', 'border-radius: 5px 5px 5px 5px;');
+    }
+    //display current amount
+    divProgressFiller.innerHTML = "<h4>" + json[i].CurrentAmount + "</h4>";
+ 
+    
+    
+}
